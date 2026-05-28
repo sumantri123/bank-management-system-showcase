0e03b107a68efa6c4dd76eb0b4ef0d7858f65b73
Sumantri <68884302+sumantri123@users.noreply.github.com>
2026-05-28 20:01:33 +0700

    Delete resources/views/layout_mobile directory

--- resources/views/layout_mobile/parts/_script.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Required jquery and libraries -->
-<script src="{{ mix('js/bankminimobile.js') }}"></script>
-<script>
-	function handleLogout(){
-		FlutterChannel.postMessage('logout');
-	}
-</script>
-@stack('scripts')
